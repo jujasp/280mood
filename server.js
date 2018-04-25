b56bf1a493a0fbe26829d401247ffd5bee0fcba1
Julia Jaspers <juliajaspers@Julias-MacBook-Pro.local>
2018-04-25 16:48:42 -0400

    some nice react boilerplate

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ const passport = require('passport')
 const TwitterStrategy = require('passport-twitter').Strategy
 const OAuth = require('OAuth')
 const Twitter = require('Twitter')
+const path = require('path')
 const secrets = require('./secrets.js')
 
 const strategy = new TwitterStrategy({
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
-    consumerLey: process.env.TWITTER_CONSUMER_SECRET,
+    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
     callbackURL: process.env.TWITTER_CALLBACK_URL
+}, (token, tokenSecret, profile, done) => {
+    return done(null, profile, {access_token_key: token, access_token_secret: tokenSecret})
 })
 
-passport.use(strategy, (token, tokenSecret, profile, done) => {
-    return done(null, profile)
-})
+passport.use(strategy)
 
 passport.serializeUser((user, done) => {
     done(null, user);
@@ -27,14 +28,17 @@ const app = express()
 
 app.use(require('morgan')('dev'))
 app.use(require('cookie-parser')())
-app.use(require('body-parser')())
+app.use(require('body-parser').urlencoded({extended: true}))
 app.use(require('express-session')({secret: 'blossombellascskye', resave: true, saveUninitialized: true}))
 
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use(express.static(path.join(__dirname, '..', 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+    console.log(__dirname)
+    res.sendFile(path.join(__dirname, 'public/index.html'))
 })
+
+app.listen(3000, () => console.log('doing all good things on 3000'))
